3ec5d609cf7ac2820fac4bd63118f91ca31b91e9
Coral <coralruschak@gmail.com>
2024-12-22 22:32:31 -0500

    Tests: Dealership tests updated for Iteration 3

--- car_dealership/spec/dealership_spec.rb
+++ car_dealership/spec/dealership_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe Dealership do
     @dealership = Dealership.new("Acme Auto", "123 Main Street")
     @car_1 = Car.new("Ford Mustang", 1500, 36)
     @car_2 = Car.new("Toyota Prius", 1000, 48)
+    @car_3 = Car.new("Toyota Tercel", 500, 48)
+    @car_4 = Car.new("Chevrolet Bronco", 1250, 24)
   end
   
   describe '#initialize' do
@@ -29,9 +31,11 @@ RSpec.describe Dealership do
       expect(@dealership.inventory).to eq([])
     end
 
-    it 'default inventory_count is 0' do
+    it 'inventory_count to default 0' do
       expect(@dealership.inventory_count).to eq(0)
     end
+
+    it 'has_inventory to default false'
 
     it 'add_car adds to inventory' do
       @dealership.add_car(@car_1)
@@ -46,5 +50,33 @@ RSpec.describe Dealership do
 
       expect(@dealership.inventory_count).to eq(2)
     end
+  end
+
+  it 'sorts cars_by_make' do
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
+
+    expect(@dealership.cars_by_make("Toyota")).to eq([@car_2, @car_3])
+    expect(@dealership.cars_by_make("Ford")).to eq([@car_1])
+  end
+
+  it 'calculates total_value of inventory' do
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
+
+    expect(@dealership.total_value).to eq([15600])
+  end
+
+  it 'provides hash with total_value & address' do
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
+    
+    expect(@dealership.details).to eq({"total_value" => 156000, "address" => "123 Main Street"})
   end
 end
