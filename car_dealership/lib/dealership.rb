56ec5208bd2c2965f36973bb94558e2f5faca61e
Coral <coralruschak@gmail.com>
2024-12-22 23:21:59 -0500

    Feat: cars_by_make & total_value created & pass tests

--- car_dealership/lib/dealership.rb
+++ car_dealership/lib/dealership.rb
@@ -14,4 +14,26 @@ class Dealership
   def inventory_count
     @inventory.count
   end
+
+  def has_inventory?
+    if inventory_count == 0
+      false
+    end
+  end
+
+  def cars_by_make(car_make)
+    @inventory.find_all do |car|
+      car.make == car_make
+    end    
+  end
+
+  def total_value
+    @inventory.sum do |car|
+      car.total_cost
+    end
+  end
+
+  def details
+    
+  end
 end
