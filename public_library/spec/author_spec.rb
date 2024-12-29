6685bc6bc1db36a8a0b9ae3cb098d68143722b52
Coral <coralruschak@gmail.com>
2024-12-29 00:58:52 -0500

    Update author_spec.rb
    All tests created from the info provided in the interaction pattern

--- public_library/spec/author_spec.rb
+++ public_library/spec/author_spec.rb
@@ -2,30 +2,60 @@ require './lib/book'
 require './lib/author'
 
 RSpec.describe Author do
-  
+    before(:each) do
+        @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    end
+    
+    describe '#initilize' do
+        it 'exists' do
+            expect(@charlotte_bronte).to_be Author
+        end
+        
+        it 'has first_name' do
+            expect(@charlotte_bronte.first_name).to eq("Charlotte")
+        end
+        
+        it 'has last_name' do
+            expect(@charlotte_bronte.last_name).to eq("Bronte")
+        end
+        
+        it 'has an empty array for books' do
+            expect(@charlotte_bronte.books).to eq([])
+        end
+    end
+    
+    describe '#name' do
+        it 'combines first_name and last_name' do
+            expect(@charlotte_bronte.name).to eq("Charlotte Bronte")
+        end
+    end
+    
+    describe '#write' do
+        it 'creates Book object by Author & Book methods can be accessed' do
+            jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+            
+            expect(jane_eyre.class).to_be_a Book
+            expect(jane_eyre.title).to eq("Jane Eyre")
+        end
+        
+        it 'adds Book to Authors book array' do
+            jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+            
+            expect(@charlotte_bronte.books).to eq([jane_eyre])
+            expect(jane_eyre.title).to eq("Jane Eyre")
+        end
+        
+        it 'tracks multiple books written by Author' do
+            jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+            villette = charlotte_bronte.write("Villette", "1853")
+            
+            expect(@charlotte_bronte.books).to eq([jane_eyre, villette])
+        end
+    end
 end
 
 
-charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-#<Author:0x00007fb898081850...>
 
-charlotte_bronte.name
-"Charlotte Bronte"
 
-charlotte_bronte.books
-[]
 
-jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-#<Book:0x00007fb896e22538...>
 
-jane_eyre.class
-Book
-
-jane_eyre.title
-"Jane Eyre"
-
-villette = charlotte_bronte.write("Villette", "1853")
-#<Book:0x00007fb8980aaca0...>
-
-charlotte_bronte.books
-[#<Book:0x00007fb896e22538...>, #<Book:0x00007fb8980aaca0...>]
